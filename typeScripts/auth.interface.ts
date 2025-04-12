b541dcf5082279711e9c1add07a4f60cfd515ace
pkm <pradosh.bargad@gmail.com>
2025-04-12 21:09:36 +0530

    Test

--- typeScripts/auth.interface.ts
+++ typeScripts/auth.interface.ts
@@ -3,7 +3,7 @@ export interface IloginProps {
     password: string,
     token: string,
     message: string,
-    status: string
+    status: boolean
 }
 export interface IregisterProps {
     name: string,
@@ -13,13 +13,13 @@ export interface IregisterProps {
     photo: FileList,
     token: string,
     message: string,
-    status: string
+    status: boolean
 }
 export interface IverifyProps {
     email: string,
     password: string,
     token: string,
-    status: string,
+    status: boolean,
     message: string,
 }
 export interface loginProps extends IloginProps {
